302ae70b53e42c3ed3aea970457623cd9697c8ec
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2022-05-09 00:19:21 +0200

    dev paginator

--- src/components/PostsList.vue
+++ src/components/PostsList.vue
@@ -1,9 +1,38 @@
 <script lang="ts" setup>
-import { getArticles } from "~/api"
+import { Article, getArticles } from "~/api"
+import { pagination } from "~/helpers/pagination-range"
 
-const { data } = await getArticles({})
+const limit = 10
 
-const articles = data.articles
+let articles = ref<Article[]>([])
+
+const { data } = await getArticles({ limit })
+
+articles.value = data.articles!
+
+const fetchData = async ({
+  currentPage,
+  currentPageSize,
+}: {
+  currentPage: number
+  currentPageSize: number
+}) => {
+  const { data } = await getArticles({
+    limit,
+    offset: Math.floor(currentPageSize * (currentPage - 1)),
+  })
+
+  articles.value = data.articles!
+}
+
+const { currentPage, pageCount, isFirstPage, isLastPage, prev, next } =
+  useOffsetPagination({
+    total: data.articlesCount,
+    page: 1,
+    pageSize: limit,
+    onPageChange: fetchData,
+    onPageSizeChange: fetchData,
+  })
 </script>
 
 <template>
@@ -12,4 +41,33 @@ const articles = data.articles
     :key="article.slug!"
     :article="article"
   />
+
+  <div mt-4 flex flex-wrap gap-1>
+    <button :disabled="isFirstPage" class="btn" @click="prev">&lt;</button>
+    <button
+      v-for="item in pagination(currentPage, pageCount)"
+      :key="item"
+      :disabled="currentPage === item || !Number.isInteger(item)"
+      class="btn"
+      :class="{ active: currentPage === item }"
+      @click="currentPage = (item as number)"
+    >
+      {{ item }}
+    </button>
+    <button :disabled="isLastPage" class="btn" @click="next">&gt;</button>
+  </div>
 </template>
+
+<style scoped>
+.btn {
+  @apply flex items-center justify-center border rounded-1 text-sm font-sans text-gray-500 border-gray-500 w-8 h-8;
+}
+
+.btn:disabled:not(.active) {
+  @apply opacity-50;
+}
+
+.btn.active {
+  @apply text-white border-green bg-green;
+}
+</style>
---
+++ src/helpers/pagination-range.ts
@@ -0,0 +1,48 @@
+const getRange = (start: number, end: number) => {
+  return Array(end - start + 1)
+    .fill(null)
+    .map((v, i) => i + start)
+}
+
+const pagination = (currentPage: number, pageCount: number) => {
+  let delta: number
+
+  if (pageCount <= 7) {
+    delta = 7
+  } else {
+    delta = currentPage > 4 && currentPage < pageCount - 3 ? 2 : 4
+  }
+
+  const range = {
+    start: Math.round(currentPage - delta / 2),
+    end: Math.round(currentPage + delta / 2),
+  }
+
+  if (range.start - 1 === 1 || range.end + 1 === pageCount) {
+    range.start += 1
+    range.end += 1
+  }
+
+  let pages: any =
+    currentPage > delta
+      ? getRange(
+          Math.min(range.start, pageCount - delta),
+          Math.min(range.end, pageCount)
+        )
+      : getRange(1, Math.min(pageCount, delta + 1))
+
+  const withDots = (value: number, pair: (number | string)[]) =>
+    pages.length + 1 !== pageCount ? pair : [value]
+
+  if (pages[0] !== 1) {
+    pages = withDots(1, [1, "..."]).concat(pages)
+  }
+
+  if (pages[pages.length - 1] < pageCount) {
+    pages = pages.concat(withDots(pageCount, ["...", pageCount]))
+  }
+
+  return pages
+}
+
+export { pagination }
