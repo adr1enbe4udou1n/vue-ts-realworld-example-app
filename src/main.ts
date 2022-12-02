3250095148b05097d79214888f523252dcc11b1c
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2022-12-02 22:08:24 +0100

    wtf

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { createPinia } from "pinia"
 
 import App from "./App.vue"
 
+import "@unocss/reset/tailwind.css"
 import "./styles/main.css"
 import "uno.css"
 import { router } from "./plugins/router"
