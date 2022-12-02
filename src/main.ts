ba358ae0817a16c25aa98470dafc7a0f9e1fbc32
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2022-12-02 22:04:59 +0100

    import bug

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { createPinia } from "pinia"
 
 import App from "./App.vue"
 
-import "@unocss/reset/tailwind.css"
 import "./styles/main.css"
 import "uno.css"
 import { router } from "./plugins/router"
