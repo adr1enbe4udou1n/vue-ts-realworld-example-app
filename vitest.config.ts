11ba93d19fc6ac5f606e4665acdb9a899ab84ff0
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-05-09 15:46:35 +0200

    up

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,9 @@ export default mergeConfig(
       environment: "jsdom",
       exclude: [...configDefaults.exclude, "e2e/*"],
       root: fileURLToPath(new URL("./", import.meta.url)),
+      coverage: {
+        reporter: ["text", "lcov"],
+      },
     },
   }),
 )
