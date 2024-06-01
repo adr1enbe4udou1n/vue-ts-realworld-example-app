08ef211e1cb92a8d9001d03c78789b8cce96c0c1
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-06-01 12:58:31 +0200

    up

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router/auto"
+import { routes } from "vue-router/auto-routes"
 
 import NProgress from "nprogress"
 
@@ -6,6 +7,7 @@ import { useUserStore } from "../stores/user.js"
 
 const router = createRouter({
   history: createWebHistory(),
+  routes,
 })
 
 router.beforeEach(async (to, from) => {
